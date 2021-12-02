fe5bcd197baad2720c444a0835ee90acf3052a52
david <davidias.j.a@gmail.com>
2021-12-02 12:20:14 -0500

    Shading functions now work

--- lib/lighting.c
+++ lib/lighting.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include "graphicslib.h"
 
 /* LIGHT FUNCTIONS */
@@ -46,7 +47,7 @@ Lighting *lighting_create(void) {
     Lighting *lights = (Lighting *) malloc(sizeof(Lighting));
     if (!lights) {
         printf("lighting_create(): malloc failed. Returning.\n");
-        return;
+        return NULL;
     }
 
     lighting_init(lights);
@@ -127,14 +128,13 @@ void lighting_shading(Lighting *l, Vector *N, Vector *V, Point *p, Color *Cb,
         return;
     }
 
-    // If point is one-sided and we can't see it's normal, we can just ignore it
-    if (oneSided != 0 && vector_dot(V, N) <= 0) {
-        return;                
-    }
 
+    color_set(c, 0, 0, 0);
     Vector L = {{0.0, 0.0, 0.0}}; // Placeholder for L-vector
     Vector negative_L = {{0.0, 0.0, 0.0}}; // Used in spotlighting
     Vector H = {{0.0, 0.0, 0.0}}; // Placeholder for H-vector
+    float l_dot_n, pow_s_of_H_dot_N;
+    printf("Initial color: (%.2f %.2f %.2f)\n", c->c[0], c->c[1], c->c[2]);
 
     // evaluate lighting for every light at point p with passed params:
     for (int i = 0; i < l->nLights; i++) {
@@ -147,51 +147,89 @@ void lighting_shading(Lighting *l, Vector *N, Vector *V, Point *p, Color *Cb,
                 c->c[0] = c->c[0] + Cb->c[0] * light.color.c[0]; // Set red
                 c->c[1] = c->c[1] + Cb->c[1] * light.color.c[1]; // Set green
                 c->c[2] = c->c[2] + Cb->c[2] * light.color.c[2]; // Set blue
+                printf("Ambient light: (%.2f %.2f %.2f)\n",\
+                    c->c[0], c->c[1], c->c[2]);
                 break;
             
             case LightDirect:
+                // If point is one-sided and we can't see it's normal, we can 
+                // just ignore it
+                if (oneSided != 0 && vector_dot(V, N) <= 0) {
+                    break;                
+                }
                 // In the case of direct lighting, the light is positionless
                 // (or more accurately, it's located at infinity along the
                 // vector light.direction). So, we just copy the light direction
                 // into L:
                 vector_copy(&L, &light.direction);
-                vector_set(&H, L.val[0] + V->val[0] / 2,  
-                               L.val[1] + V->val[1] / 2,
-                               L.val[2] + V->val[2] / 2);
-                
+
+                // If the view is on the opposite side of the point as the light
+                // don't shade the point.
+                if (vector_dot(V, &L) <= 0) {
+                    break;
+                }
+
+                vector_set(&H, (L.val[0] + V->val[0]) / 2.0,  
+                               (L.val[1] + V->val[1]) / 2.0,
+                               (L.val[2] + V->val[2]) / 2.0);
+                vector_normalize(&L);
+                vector_normalize(&H);
+
                 // Update color accumulator using light equation:
+                l_dot_n = fabs(vector_dot(&L, N));
+                pow_s_of_H_dot_N = pow(vector_dot(&H, N), s);
                 c->c[0] =   c->c[0] + 
-                          (Cb->c[0] * light.color.c[0] * vector_dot(&L, N) + 
-                           Cs->c[0] * light.color.c[0] * vector_dot(&H, N));
+                          (Cb->c[0] * light.color.c[0] * l_dot_n + 
+                           Cs->c[0] * light.color.c[0] * pow_s_of_H_dot_N);
                 c->c[1] =   c->c[1] + 
-                          (Cb->c[1] * light.color.c[1] * vector_dot(&L, N) + 
-                           Cs->c[1] * light.color.c[1] * vector_dot(&H, N));
+                          (Cb->c[1] * light.color.c[1] * l_dot_n + 
+                           Cs->c[1] * light.color.c[1] * pow_s_of_H_dot_N);
                 c->c[2] =   c->c[2] + 
-                          (Cb->c[2] * light.color.c[2] * vector_dot(&L, N) + 
-                           Cs->c[2] * light.color.c[2] * vector_dot(&H, N));
+                          (Cb->c[2] * light.color.c[2] * l_dot_n + 
+                           Cs->c[2] * light.color.c[2] * pow_s_of_H_dot_N);                printf("Direct light: (%.2f %.2f %.2f)\n",\
+                    c->c[0], c->c[1], c->c[2]);
                 break;
             
             case LightPoint:
+                // If point is one-sided and we can't see it's normal, we can just ignore it
+                if (oneSided != 0 && vector_dot(V, N) <= 0) {
+                    break;                
+                }
+
                 // This only differs from directional light in that we need to
                 // calculate the L vector based on the Point we are coloring and
                 // the light source location.
                 vector_set(&L, light.position.val[0] - p->val[0],
                                light.position.val[1] - p->val[1],
                                light.position.val[2] - p->val[2]);
-                vector_set(&H, L.val[0] + V->val[0] / 2,  
-                               L.val[1] + V->val[1] / 2,
-                               L.val[2] + V->val[2] / 2);
+
+                // If the view is on the opposite side of the point as the light
+                // don't shade the point.
+                if (vector_dot(V, &L) <= 0) {
+                    break;
+                }
+                
+                // Set halfway vector:
+                vector_set(&H, (L.val[0] + V->val[0]) / 2.0,  
+                               (L.val[1] + V->val[1]) / 2.0,
+                               (L.val[2] + V->val[2]) / 2.0);
+                vector_normalize(&L);
+                vector_normalize(&H);
 
                 // Update color accumulator using light equation:
+                l_dot_n = fabs(vector_dot(&L, N));
+                pow_s_of_H_dot_N = pow(vector_dot(&H, N), s);
                 c->c[0] =   c->c[0] + 
-                          (Cb->c[0] * light.color.c[0] * vector_dot(&L, N) + 
-                           Cs->c[0] * light.color.c[0] * vector_dot(&H, N));
+                          (Cb->c[0] * light.color.c[0] * l_dot_n + 
+                           Cs->c[0] * light.color.c[0] * pow_s_of_H_dot_N);
                 c->c[1] =   c->c[1] + 
-                          (Cb->c[1] * light.color.c[1] * vector_dot(&L, N) + 
-                           Cs->c[1] * light.color.c[1] * vector_dot(&H, N));
+                          (Cb->c[1] * light.color.c[1] * l_dot_n + 
+                           Cs->c[1] * light.color.c[1] * pow_s_of_H_dot_N);
                 c->c[2] =   c->c[2] + 
-                          (Cb->c[2] * light.color.c[2] * vector_dot(&L, N) + 
-                           Cs->c[2] * light.color.c[2] * vector_dot(&H, N));
+                          (Cb->c[2] * light.color.c[2] * l_dot_n + 
+                           Cs->c[2] * light.color.c[2] * pow_s_of_H_dot_N);
+                printf("Point light: (%.2f %.2f %.2f)\n",\
+                    c->c[0], c->c[1], c->c[2]);
                 break;
             
             case LightSpot:
@@ -202,7 +240,42 @@ void lighting_shading(Lighting *l, Vector *N, Vector *V, Point *p, Color *Cb,
                 vector_set(&H, L.val[0] + V->val[0] / 2,  
                                L.val[1] + V->val[1] / 2,
                                L.val[2] + V->val[2] / 2);
-                
+                vector_set(&negative_L, L.val[0], L.val[1], L.val[2]);
+                vector_normalize(&L);
+                vector_normalize(&H);
+
+                // Calculate cos of angle between negative_L and light direction
+                float alpha = vector_dot(&light.direction, &negative_L) / 
+                              (vector_length(&light.direction) * 
+                               vector_length(&negative_L));
+                l_dot_n = fabs(vector_dot(&L, N));
+                pow_s_of_H_dot_N = pow(vector_dot(&H, N), s);
+
+                // If alpha is greater than the cutoff angle, we apply the light
+                // equation but with a falloff function applied:
+                if (alpha > light.cutoff) {
+                    int falloff = pow(alpha, light.sharpness);
+                    c->c[0] = c->c[0] + falloff *
+                            (Cb->c[0] * light.color.c[0] * l_dot_n + 
+                             Cs->c[0] * light.color.c[0] * pow_s_of_H_dot_N);
+                    c->c[1] = c->c[1] + falloff *
+                            (Cb->c[1] * light.color.c[1] * l_dot_n + 
+                             Cs->c[1] * light.color.c[1] * pow_s_of_H_dot_N);
+                    c->c[2] = c->c[2] + falloff *
+                            (Cb->c[2] * light.color.c[2] * l_dot_n + 
+                             Cs->c[2] * light.color.c[2] * pow_s_of_H_dot_N);
+                } else {
+                    // Update color accumulator using light equation:
+                    c->c[0] =   c->c[0] + 
+                              (Cb->c[0] * light.color.c[0] * l_dot_n + 
+                               Cs->c[0] * light.color.c[0] * pow_s_of_H_dot_N);
+                    c->c[1] =   c->c[1] + 
+                              (Cb->c[1] * light.color.c[1] * l_dot_n + 
+                               Cs->c[1] * light.color.c[1] * pow_s_of_H_dot_N);
+                    c->c[2] =   c->c[2] + 
+                              (Cb->c[2] * light.color.c[2] * l_dot_n + 
+                               Cs->c[2] * light.color.c[2] * pow_s_of_H_dot_N);
+                    }
                 break;
             
             default:
@@ -211,4 +284,12 @@ void lighting_shading(Lighting *l, Vector *N, Vector *V, Point *p, Color *Cb,
 
         }
     }
+
+    // Clip c to [0,1]:
+    if (c->c[0] < 0.0) c->c[0] = 0.0;
+    if (c->c[1] < 0.0) c->c[1] = 0.0;
+    if (c->c[2] < 0.0) c->c[2] = 0.0;
+    if (c->c[0] > 1.0) c->c[0] = 1.0;
+    if (c->c[1] > 1.0) c->c[1] = 1.0;
+    if (c->c[2] > 1.0) c->c[2] = 1.0;
 }
